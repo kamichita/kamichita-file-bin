22cc96b0396a3543bdab10b07c9e459ada78facf
Kamichita san <satoushogo1@gmail.com>
2025-03-27 16:50:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('uploadButton').addEventListener('click', () => {
+    const files = document.getElementById('fileInput').files;
+    if (files.length === 0) {
+        alert("ファイルを選択してください！");
+        return;
+    }
+
+    // 簡単なアップロードロジック
+    Array.from(files).forEach(file => {
+        console.log(`Uploading: ${file.name}`);
+        // ここでアップロード処理を行う
+        const link = `https://example.com/download/${file.name}`;
+        const downloadKey = Math.random().toString(36).substr(2, 8);
+        document.getElementById('links').innerHTML += `
+            <p>ファイル名: ${file.name} <br> リンク: <a href="${link}" target="_blank">${link}</a> <br> ダウンロードキー: ${downloadKey}</p>
+        `;
+    });
+});
